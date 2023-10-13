358743865aa3ac8dbf0250932317c2fc0351e5e9
martnezcuevasjacob@gmail.com <martinezcuevasjacob@gmail.com>
2023-10-12 22:16:17 -0600

    tarea

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 
 
+// let a = 5;
+// let b = 6;
+// let resultado1 = (a + b)
+// console.log ( resultado1)
+
+//  a = 5;
+//  b = 6;
+// let resultado2 = (a / b)
+// console.log ( resultado2)
 
 
-console.log("hola koders")
+//  a = 5;
+//  b = 6;
+// let resultado3 = (a * b)
+// console.log ( resultado3)
+let a = 10
+let b = 20
+let result1 = a + b
+console.log (result1)
+
+result2 = a - b
+
+console.log (result2);
+
+// tarea   buscar una funcion para cada articulo con todo lo que conlleva la funcion // cual es la estructura de una funion 
+
