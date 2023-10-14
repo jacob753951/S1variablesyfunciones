04da065f9764dd21f8a06e697c428b390bb400eb
martnezcuevasjacob@gmail.com <martinezcuevasjacob@gmail.com>
2023-10-13 22:06:45 -0600

    avance de la funcion

--- clase2.js
+++ clase2.js
@@ -1,17 +1,54 @@
 
 
-function addTwoNombers(fistNumber, secondNumber) {
-   let result = fistNumber + secondNumber;
-   return result;
-}
+// function addTwoNombers(fistNumber, secondNumber) {
+//    let result = fistNumber + secondNumber;
+//    return result;
+// }
 
-let suma1 = addTwoNombers(6,9);
+// let suma1 = addTwoNombers(6,9);
 
-console.log(suma1);
+// console.log(suma1);
 
-function substracTwoNombers(fistNumber, secondNumber) {
-    let result = fistNumber - secondNumber;
-    return result;
- } 
- let resta1 = substracTwoNombers(8,20)
- console.log(resta1)
+// function substracTwoNombers(fistNumber, secondNumber) {
+//     let result = fistNumber - secondNumber;
+//     return result;
+//  } 
+//  let resta1 = substracTwoNombers(8,20)
+//  console.log(resta1) 
+
+//  function dividedbyTwoNombers(fistNumber, secondNumber) {
+//     let result = fistNumber / secondNumber;
+//     return result;
+//  } 
+//  let divsion1 = dividedbyTwoNombers(520,20)
+
+//  console.log(divsion1) 
+
+//  function multiplyTwoNombers(fistNumber, secondNumber) {
+//     let result = fistNumber / secondNumber;
+//     return result;
+//  } 
+//  let multiplicacion1  = multiplyTwoNombers (2,20)
+
+//  console.log(multiplicacion1) 
+
+
+ function esParOImpar(numero) {
+    if (numero % 2 === 0) {
+      return "Par";
+    } else {
+      return "Impar";
+    }
+  }
+  
+  let numeroIngresado = prompt("Por favor, ingrese un número:");
+  let numero = parseInt(numeroIngresado);
+  
+  if (!isNaN(numero))
+   { 
+    let resultado = esParOImpar(numero);
+    console.log(`El número ${numero} es ${resultado}.`);
+  } 
+  else {
+    console.log("Por favor, ingrese un número válido.");
+  }
