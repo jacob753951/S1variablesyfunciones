b5b808f574fb3cc968166555aff7d57f253cca92
martnezcuevasjacob@gmail.com <martinezcuevasjacob@gmail.com>
2023-10-13 21:50:03 -0600

    funciones

---
+++ clase2.js
@@ -0,0 +1,17 @@
+
+
+function addTwoNombers(fistNumber, secondNumber) {
+   let result = fistNumber + secondNumber;
+   return result;
+}
+
+let suma1 = addTwoNombers(6,9);
+
+console.log(suma1);
+
+function substracTwoNombers(fistNumber, secondNumber) {
+    let result = fistNumber - secondNumber;
+    return result;
+ } 
+ let resta1 = substracTwoNombers(8,20)
+ console.log(resta1)
